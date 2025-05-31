75735ed92406b1236e7587bf667a4cbfd7a6d874
uhbbang3 <ansdnjswjd33@gmail.com>
2025-05-31 18:47:27 +0900

    [Feat] 플레이어 움직임, Input System 적용
    Input System 이용해 플레이어 움직임 구현
    (WASD키, 화살표 방향키 모두 사용 가능
    
    PlayerMove.cs 생성
    Player Object에 Rigidbody 2D, BoxCollider 2D, Player Input 적용

---
+++ Assets/01 Scripts/Player/PlayerMove.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+    private InputActions _inputActions;
+    private Vector2 _moveDirection;
+    private Rigidbody2D _rb;
+
+    [SerializeField] private float _moveSpeed;
+
+    private void Awake()
+    {
+        _moveDirection = Vector2.zero;
+        _inputActions = new InputActions();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+
+        _inputActions.Player.Move.performed += OnMovePerformed;
+        _inputActions.Player.Move.canceled += OnMoveCanceled;
+
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Player.Move.performed -= OnMovePerformed;
+        _inputActions.Player.Move.canceled -= OnMoveCanceled;
+
+        _inputActions.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocity = new Vector2(_moveDirection.x * _moveSpeed, _moveDirection.y * _moveSpeed);
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        _moveDirection = Vector2.zero;
+    }
+}
