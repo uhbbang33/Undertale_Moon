86951cd7a9ce85e0a3b0aecb052234f185445223
uhbbang3 <ansdnjswjd33@gmail.com>
2025-06-05 15:36:50 +0900

    [Refactor] Move 클래스 생성
    - PlayerMove가 상속받음

---
+++ Assets/01 Scripts/Player/Move.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Move : MonoBehaviour
+{
+    private InputActions _inputActions;
+    protected Vector2 _moveDirection;
+    private Rigidbody2D _rb;
+
+    [SerializeField] private float _moveSpeed;
+
+    protected virtual void Awake()
+    {
+        _moveDirection = Vector2.zero;
+        _inputActions = new InputActions();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+
+        _inputActions.Player.Move.performed += OnMovePerformed;
+        _inputActions.Player.Move.canceled += OnMoveCanceled;
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Player.Move.performed -= OnMovePerformed;
+        _inputActions.Player.Move.canceled -= OnMoveCanceled;
+
+        _inputActions.Disable();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        _rb.linearVelocity = new Vector2(_moveDirection.x * _moveSpeed, _moveDirection.y * _moveSpeed);
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    {
+        _moveDirection = Vector2.zero;
+    }
+}
--- Assets/01 Scripts/Player/PlayerMove.cs
+++ Assets/01 Scripts/Player/PlayerMove.cs
@@ -1,43 +1,21 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : Move
 {
-    private InputActions _inputActions;
-    private Vector2 _moveDirection;
     private Vector2 _lastDirection;
-    private Rigidbody2D _rb;
     private Animator _animator;
 
-    [SerializeField] private float _moveSpeed;
-
-    private void Awake()
+    protected override void Awake()
     {
-        _moveDirection = Vector2.zero;
-        _inputActions = new InputActions();
-        _rb = GetComponent<Rigidbody2D>();
+        base.Awake();
+
         _animator = GetComponent<Animator>();
     }
 
-    private void OnEnable()
+    protected override void FixedUpdate()
     {
-        _inputActions.Enable();
-
-        _inputActions.Player.Move.performed += OnMovePerformed;
-        _inputActions.Player.Move.canceled += OnMoveCanceled;
-    }
-
-    private void OnDisable()
-    {
-        _inputActions.Player.Move.performed -= OnMovePerformed;
-        _inputActions.Player.Move.canceled -= OnMoveCanceled;
-
-        _inputActions.Disable();
-    }
-
-    private void FixedUpdate()
-    {
-        _rb.linearVelocity = new Vector2(_moveDirection.x * _moveSpeed, _moveDirection.y * _moveSpeed);
+        base.FixedUpdate();
 
         if (_moveDirection != Vector2.zero)
         {
@@ -51,15 +29,5 @@ public class PlayerMove : MonoBehaviour
         _animator.SetFloat("MoveY", _moveDirection.y);
         _animator.SetFloat("StopX", _lastDirection.x);
         _animator.SetFloat("StopY", _lastDirection.y);
-    }
-
-    private void OnMovePerformed(InputAction.CallbackContext context)
-    {
-        _moveDirection = context.ReadValue<Vector2>().normalized;
-    }
-
-    private void OnMoveCanceled(InputAction.CallbackContext context)
-    {
-        _moveDirection = Vector2.zero;
     }
 }
