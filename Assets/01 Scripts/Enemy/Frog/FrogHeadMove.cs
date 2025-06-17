60d10d38c9c37490d82869ef754020e318cef311
uhbbang3 <ansdnjswjd33@gmail.com>
2025-06-17 15:56:47 +0900

    [Rename] FrogHeadMove.cs 변수명 변경 및 폴더 변경

---
+++ Assets/01 Scripts/Enemy/Frog/FrogHeadMove.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class FrogHeadMove : MonoBehaviour
+{
+    [SerializeField] private float _movePeriod;
+    [SerializeField] private float _moveXScale;
+    [SerializeField] private float _moveYScale;
+
+    private Vector3 _startPosition;
+
+    private void Awake()
+    {
+        _startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float t = Time.time * (2 * Mathf.PI / _movePeriod);
+        float x = Mathf.Sin(t);
+        float y = Mathf.Sin(t * 2);
+
+        transform.localPosition = _startPosition + new Vector3(x, 0, 0) * _moveXScale + new Vector3(0, y, 0) * _moveYScale;
+    }
+}
